d4648855ae6ef16250ea6142e2efb41701cb6bf1
DougZheng <739168993@qq.com>
2021-02-24 19:05:52 +0800

    wrap successfulgit add -A!

--- src/game.py
+++ src/game.py
@@ -1,7 +1,33 @@
 import sys
 sys.path.append('../build')
-# from library import MCTS
-from library import NeuralNetwork
-# from library import Board
+from library import NeuralNetwork, MCTS, Board
 
-print("hello fuck")
+class Game():
+    def __init__(self):
+        pass
+    def run(self):
+        board = Board()
+        player1 = MCTS(4, 1500, 5, 3)
+        player2 = MCTS(4, 1500, 5, 3)
+        while True:
+            board.display()
+            res = board.get_result()
+            if res[0]:
+                break
+            print("now for", 'x' if board.get_cur_player() == 1 else 'o')
+            if board.get_cur_player() == 1:
+                print()
+                action = player1.get_action(board)
+                board.exec_move(action)
+                player1.update_with_move(action)
+                player2.update_with_move(action)
+            else:
+                print()
+                action = player2.get_action(board)
+                board.exec_move(action)
+                player1.update_with_move(action)
+                player2.update_with_move(action)
+        print("finish")
+
+game = Game()
+game.run()
--- src/library.i
+++ src/library.i
@@ -1,6 +1,33 @@
 %module library
+
 %{
-#include "example.h"
+#include "neural_network.h"
+#include "mcts.h"
+#include "board.h"
 %}
 
-int fact(int n);
+%include "std_vector.i"
+namespace std {
+    %template(IntVector) vector<int>;
+    %template(IntVectorVector) vector<vector<int>>;
+    %template(DoubleVector) vector<double>;
+    %template(DoubleVectorVector) vector<vector<double>>;
+}
+
+%include "std_pair.i"
+namespace std {
+    %template(BoolIntPair) pair<bool, int>;
+    %template(DoubleVectorDoublePair) pair<vector<double>, double>;
+}
+
+%include "std_string.i"
+
+%include "mcts.h"
+%include "board.h"
+
+class NeuralNetwork {
+public:
+    NeuralNetwork(std::string model_path, bool use_gpu, unsigned int batch_size);
+    ~NeuralNetwork();
+    void set_batch_size(unsigned int batch_size);
+};
