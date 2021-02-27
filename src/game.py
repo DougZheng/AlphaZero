5aa7469b09caef02df0f8a342081c2fa8a410d7a
DougZheng <739168993@qq.com>
2021-02-27 23:19:29 +0800

    test model

--- src/game.py
+++ src/game.py
@@ -1,37 +1,33 @@
 import sys
 sys.path.append('../build')
-from library import NeuralNetwork
-from library import MCTS, AlphaZero
-from library import Board
-# from neural_network import NeuralNetworkWrapper
+from library import MCTS, AlphaZero, Board, NeuralNetwork
+from neural_network import NeuralNetWorkWrapper
 
 class Game():
     def __init__(self):
         pass
     def run(self):
         board = Board(8, 5)
-        player1 = MCTS(4, 1500, 5, 3)
-        player2 = MCTS(4, 1500, 5, 3)
-        # neural_network = NeuralNetwork("../test/models/best_checkpoint.pt", False, 4)
-        # player2 = AlphaZero(neural_network, 4, 5, 5, 3)
+        player1 = MCTS(4, 800, 5, 3)
+        # player2 = MCTS(4, 1800, 5, 3)
+        neural_network = NeuralNetwork("../src/best_checkpoint.pt", False, 4)
+        player2 = AlphaZero(neural_network, 4, 800, 5, 3)
         while True:
             board.display()
             res = board.get_result()
             if res[0]:
                 break
             print("now for", 'x' if board.get_cur_player() == 1 else 'o')
+            print()
             if board.get_cur_player() == 1:
-                print()
                 action = player1.get_action(board)
                 board.exec_move(action)
-                player1.update_with_move(action)
-                player2.update_with_move(action)
             else:
-                print()
                 action = player2.get_action(board)
                 board.exec_move(action)
-                player1.update_with_move(action)
-                player2.update_with_move(action)
+            
+            player1.update_with_move(action)
+            player2.update_with_move(action)
         print("finish")
 
 game = Game()
